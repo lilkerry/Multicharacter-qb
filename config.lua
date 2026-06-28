cdac135dee3aa269b62a905c36a3f0bab4e550c1
KERRYGAMER <148004830+lilkerry@users.noreply.github.com>
2026-06-28 01:29:04 -0400

    Add initial configuration settings in config.lua

---
+++ config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+Config.CharacterSlots = 4
+Config.DefaultSpawn = vector4(-1037.71, -2737.03, 20.1693, 328.02)
+
+Config.StarterItems = {
+    { name = "phone", amount = 1 },
+    { name = "id_card", amount = 1 },
+    { name = "driver_license", amount = 1 },
+}
