9813fdf56714e35ec5428af751c43d3d0d0d07e4
KERRYGAMER <148004830+lilkerry@users.noreply.github.com>
2026-06-28 01:27:59 -0400

    Create fxmanifest for QBCore multicharacter script
    Initial setup of fxmanifest for QBCore multicharacter script with shared, client, and server scripts defined.

---
+++ fxmanifest.lua
@@ -0,0 +1,33 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'Kerrygamer'
+description 'Simple QBCore Multicharacter Script'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/app.js'
+}
+
+dependencies {
+    'qb-core'
+}
